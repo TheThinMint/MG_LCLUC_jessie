52cf905d06925970de94a7afbc5517e2b1fdbfd2
TheThinMint <jah667@cornell.edu>
2025-06-26 14:01:49 -0400

    altLife commit

--- data/2_summary_stats.R
+++ data/2_summary_stats.R
@@ -43,7 +43,7 @@ print(count_wide, n = 30)
 
 
 
-## Who undertakes migrations (labor_whoMigrates)--------------------------------
+## Who undertakes herding migrations (labor_whoMigrates)--------------------------------
 relationship_order <- c("husband", "wife", "son(s)", "daughter(s)", "child(ren), unspecified", "brother(s)", "sibling(s), unspecified", "father",
   "mother", "grandparent(s), unspecified", "grandchild(ren), unspecified", "household head", "extended family/in-laws", "friend/neighbor(s)",
   "person(s), unspecified", "hired help", "just myself", "other", "husband. 2 sons")
@@ -378,12 +378,275 @@ print(tenure_table_sprPasContract)
 
 
 
+###base_ALTLIFE-----------------------------------------------------------------
+skim(base_ALTLIFE)
+
+## Is someone in the household doing non-herding work? (altLife_nonHerdWork)----
+  #Broken up by Soum
+altLife_table_nonHerdWork <- table(base_ALTLIFE$Soum, base_ALTLIFE$altLife_nonHerdWork)
+print(altLife_table_nonHerdWork)
+
+
+
+  #If so, who is doing non-herding work? (altLife_whoNoHerdWork)
+count_whoNoHerdWork <- base_ALTLIFE %>%
+  select(Soum, altLife_whoNoHerdwork) %>%
+  filter(!is.na(altLife_whoNoHerdwork)) %>%
+  separate_rows(altLife_whoNoHerdwork, sep = ",") %>%
+  mutate(
+    altLife_whoNoHerdwork = str_trim(altLife_whoNoHerdwork),
+    altLife_whoNoHerdwork = str_to_lower(altLife_whoNoHerdwork),
+    altLife_whoNoHerdwork = case_when(
+      altLife_whoNoHerdwork == "son-in-law" ~ "extended family members",
+      altLife_whoNoHerdwork == "doctor" ~ "not specified",
+      TRUE ~ altLife_whoNoHerdwork
+    )
+  ) %>% 
+  count(Soum, altLife_whoNoHerdwork, sort = TRUE)
+
+count_whoNoHerdWork <- count_whoNoHerdWork %>%
+  pivot_wider(
+    names_from = Soum,
+    values_from = n,
+    values_fill = 0  # Fill missing values with 0s
+  ) %>%
+  rowwise() %>%
+  mutate(Total = sum(c_across(where(is.numeric)))) %>%
+  ungroup()
+
+print(count_whoNoHerdWork, n = 30)
+
+
+
+  #If so, what is that work? (altLife_noHerdWhatWork)
+count_noHerdWhatWork <- base_ALTLIFE %>%
+  select(Soum, altLife_noHerdWhatWork) %>%
+  filter(!is.na(altLife_noHerdWhatWork)) %>%
+  separate_rows(altLife_noHerdWhatWork, sep = ",") %>%
+  mutate(
+    altLife_noHerdWhatWork = str_trim(altLife_noHerdWhatWork),
+    altLife_noHerdWhatWork = str_to_lower(altLife_noHerdWhatWork),
+    altLife_noHerdWhatWork = case_when(
+      altLife_noHerdWhatWork == "bagh governor" ~ "government leadership",
+      altLife_noHerdWhatWork == "public service" ~ "government employment",
+      altLife_noHerdWhatWork == "employment at soum governor office" ~ "government employment",
+      altLife_noHerdWhatWork == "public servant" ~ "government employment",
+      altLife_noHerdWhatWork == "soum culture center" ~ "government employment",
+      altLife_noHerdWhatWork == "dance instructor at culture center" ~ "government employment",
+      altLife_noHerdWhatWork == "soum center" ~ "government employment",
+      altLife_noHerdWhatWork == "procurement at the governor office" ~ "government employment",
+      altLife_noHerdWhatWork == "social worker in ulaanbaatar hospital" ~ "government employment",
+      altLife_noHerdWhatWork == "employment in environmental agency" ~ "government employment",
+      altLife_noHerdWhatWork == "mining" ~ "mining and construction",
+      altLife_noHerdWhatWork == "employment in mining" ~ "mining and construction",
+      altLife_noHerdWhatWork == "part-time employment in mining" ~ "mining and construction",
+      altLife_noHerdWhatWork == "drives truck in mining" ~ "mining and construction",
+      altLife_noHerdWhatWork == "employment in construction" ~ "mining and construction",
+      altLife_noHerdWhatWork == "metal work (sometimes)" ~ "mining and construction",
+      altLife_noHerdWhatWork == "plumber" ~ "mining and construction",
+      altLife_noHerdWhatWork == "driver" ~ "mining and construction",
+      altLife_noHerdWhatWork == "employment in restaurant" ~ "commerce-related and restaurants",
+      altLife_noHerdWhatWork == "cook at school kitchen" ~ "commerce-related and restaurants",
+      altLife_noHerdWhatWork == "retail business" ~ "commerce-related and restaurants",
+      altLife_noHerdWhatWork == "cook at kindergarten" ~ "commerce-related and restaurants",
+      altLife_noHerdWhatWork == "cashier at grocery store" ~ "commerce-related and restaurants",
+      altLife_noHerdWhatWork == "part-time cashier at supermarket" ~ "commerce-related and restaurants",
+      altLife_noHerdWhatWork == "any part-time job" ~ "commerce-related and restaurants",
+      altLife_noHerdWhatWork == "running store" ~ "commerce-related and restaurants",
+      altLife_noHerdWhatWork == "working at supermarket" ~ "commerce-related and restaurants",
+      altLife_noHerdWhatWork == "commercial service" ~ "commerce-related and restaurants",
+      altLife_noHerdWhatWork == "work at supermarket" ~ "commerce-related and restaurants",
+      altLife_noHerdWhatWork == "private business" ~ "commerce-related and restaurants",
+      altLife_noHerdWhatWork == "kindergarten teacher" ~ "education",
+      altLife_noHerdWhatWork == "school teacher" ~ "education",
+      altLife_noHerdWhatWork == "driving instructor" ~ "education",
+      altLife_noHerdWhatWork == "assistant teacher at kindergarten" ~ "education",
+      altLife_noHerdWhatWork == "studies in japan" ~ "education",
+      altLife_noHerdWhatWork == "teacher at kindergarten" ~ "education",
+      altLife_noHerdWhatWork == "subsidy for wool/hide" ~ "agriculture and pastoralist-adjacent",
+      altLife_noHerdWhatWork == "working the hide" ~ "agriculture and pastoralist-adjacent",
+      altLife_noHerdWhatWork == "crop farming" ~ "agriculture and pastoralist-adjacent",
+      altLife_noHerdWhatWork == "dairy store" ~ "agriculture and pastoralist-adjacent",
+      altLife_noHerdWhatWork == "sales from dairy products" ~ "agriculture and pastoralist-adjacent",
+      altLife_noHerdWhatWork == "employment" ~ "employment unspecified",
+      altLife_noHerdWhatWork == "in ulaanbaatar" ~ "employment unspecified",
+      altLife_noHerdWhatWork == "pension" ~ "employment unspecified",
+      altLife_noHerdWhatWork == "salary" ~ "employment unspecified",
+      altLife_noHerdWhatWork == "grandmother" ~ "employment unspecified",
+      altLife_noHerdWhatWork == "employed" ~ "employment unspecified",
+      altLife_noHerdWhatWork == "knitting" ~ "arts, crafts, and handwork",
+      altLife_noHerdWhatWork == "horse archery" ~ "arts, crafts, and handwork",
+      altLife_noHerdWhatWork == "film director" ~ "arts, crafts, and handwork",
+      altLife_noHerdWhatWork == "tailoring" ~ "arts, crafts, and handwork",
+      altLife_noHerdWhatWork == "doctor" ~ "medical/veterinary",
+      altLife_noHerdWhatWork == "veterinarian" ~ "medical/veterinary",
+      altLife_noHerdWhatWork == "cleaner" ~ "mischellaneous",
+      altLife_noHerdWhatWork == "head of the soum disabled community" ~ "mischellaneous",
+      altLife_noHerdWhatWork == "security" ~ "mischellaneous",
+      TRUE ~ altLife_noHerdWhatWork
+    )
+  ) %>% 
+  count(Soum, altLife_noHerdWhatWork, sort = TRUE)
+
+count_noHerdWhatWork <- count_noHerdWhatWork %>%
+  pivot_wider(
+    names_from = Soum,
+    values_from = n,
+    values_fill = 0  # Fill missing values with 0s
+  ) %>%
+  rowwise() %>%
+  mutate(Total = sum(c_across(where(is.numeric)))) %>%
+  ungroup()
+print(count_noHerdWhatWork, n = 60)
+
+
+
+
+## Additional sources of income for the household? (altLife_otherInc)-----------
+  #Broken up by Soum
+count_otherInc <- base_ALTLIFE %>%
+  select(Soum, altLife_otherInc) %>%
+  filter(!is.na(altLife_otherInc)) %>%
+  separate_rows(altLife_otherInc, sep = ",") %>%
+  mutate(
+    altLife_otherInc = str_trim(altLife_otherInc),
+    altLife_otherInc = str_to_lower(altLife_otherInc),
+    altLife_otherInc = case_when(
+      altLife_otherInc == "bagh governor" ~ "government leadership",
+      TRUE ~ altLife_otherInc
+    )
+  ) %>% 
+  count(Soum, altLife_otherInc, sort = TRUE)
+
+count_otherInc <- count_otherInc %>%
+  pivot_wider(
+    names_from = Soum,
+    values_from = n,
+    values_fill = 0  # Fill missing values with 0s
+  ) %>%
+  rowwise() %>%
+  mutate(Total = sum(c_across(where(is.numeric)))) %>%
+  ungroup()
+print(count_otherInc, n = 60)
+
+
+
+
+## Typical number of loans per year? (altLife_loansPerYr)-----------------------
+altLife_table_loansPerYr <- table(base_ALTLIFE$Soum, base_ALTLIFE$altLife_loansPerYr)
+print(altLife_table_loansPerYr)
+
+
+## Loan Sizes (min, max, mean, median, range) (altLife_loansMin/altLife_loansMax)
+  #Broken up by Soum
+  #Summaries for separate columns: 
+base_loans <- base_ALTLIFE %>%
+  select(Soum, altLife_loansMin, altLife_loansMax) %>%
+  group_by(Soum) %>%  # Group by Soum before summarizing
+  summarise(
+    min_minLoan = min(altLife_loansMin, na.rm = TRUE),
+    max_minLoan = max(altLife_loansMin, na.rm = TRUE),
+    mean_minLoan = mean(altLife_loansMin, na.rm = TRUE),
+    median_minLoan = median(altLife_loansMin, na.rm = TRUE),
+    min_maxLoan = min(altLife_loansMax, na.rm = TRUE),
+    max_maxLoan = max(altLife_loansMax, na.rm = TRUE),
+    mean_maxLoan = mean(altLife_loansMax, na.rm = TRUE),
+    median_maxLoan = median(altLife_loansMax, na.rm = TRUE)
+    )
+print(base_loans)
 
 
 
 
 
+## When do you typically need loans? (altLife_loansWhenNeed)--------------------
+  #Broken up by Soum
+count_loansWhenNeed <- base_ALTLIFE %>%
+  select(Soum, altLife_loansWhenNeed) %>%
+  filter(!is.na(altLife_loansWhenNeed)) %>%
+  mutate(
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "Winter and spring", "winter, spring"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "Winter and Spring", "winter, spring"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "august- for school year and lunar new year", "autumn, lunar new year"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "Winter-fodder", "winter"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "winter-fodder", "winter"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "Winter- require fodder", "winter"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "and Winter", "winter"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "winter and petrol", "winter"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "february", "winter"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "February", "winter"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "After December", "winter"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "Winter and preparation of fodder", "winter"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "Dec", "winter"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "december", "winter"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "winter-winter for fodder", "winter"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "winterember", "winter"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "Winter Nov-March", "winter"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "winter and spring", "winter, spring"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "winter and Lunar new year", "winter, lunar new year"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "Winter and Lunar new year", "winter, lunar new year"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "Winter and Lunar New Year", "winter, lunar new year"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "spring and autumn", "spring, autumn"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "spring-fodder", "spring"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "Spring- before cashmere collection", "spring"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "Lunar new year-spring", "spring, lunar new year"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "summer and winter", "summer, winter"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "Sep-fodder", "autumn"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "autumn-fodder", "autumn"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "prepare fodder in Autumn", "autumn"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "August", "autumn"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "Autumn- school year", "autumn"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "Autumn- school new year", "autumn"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "Aug", "autumn"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "autumn-sep", "autumn"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "Autumn-sep", "autumn"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "October", "autumn"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "September", "autumn"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "Autumn- student tuition fee payment and buying fodder", "autumn"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "Autumn- school year starts", "autumn"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "september-school year", "autumn"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "Autumn-school new year", "autumn"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "Autumn-school year starts", "autumn"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "autumn -sep", "autumn"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "autumn- for school year and lunar new year", "autumn, lunar new year"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "Autumn and Lunar New Year", "autumn, lunar new year"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "autumn and during lunar new year", "autumn, lunar new year"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "Lunar New Year and Sept", "autumn, lunar new year"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "autumn and spring-lunar new year", "autumn, lunar new year, spring"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "Autum and winter", "autumn, winter"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "september and during harsh winter", "autumn, winter"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "autumn and winter", "autumn, winter"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "Lunar New year and need of fodder", "lunar new year"), 
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "If livestock is weak", "depends on needs"), 
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "depends on demands", "depends on needs"), 
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "0", "never"), 
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "None", "never"), 
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "none", "never"),
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "During migration and need of fodder", "during migration, depends on needs"), 
+    altLife_loansWhenNeed = str_replace(altLife_loansWhenNeed, "Autumn and during medical treatment", "autumn, during medical treatment")
+  ) %>%
+  separate_rows(altLife_loansWhenNeed, sep = ",") %>%
+  mutate(
+    altLife_loansWhenNeed = str_trim(altLife_loansWhenNeed),
+    altLife_loansWhenNeed = str_to_lower(altLife_loansWhenNeed),
+    altLife_loansWhenNeed = case_when(
+      altLife_loansWhenNeed == "bagh governor" ~ "government leadership",
+      TRUE ~ altLife_loansWhenNeed
+    )
+  ) %>% 
+  count(Soum, altLife_loansWhenNeed, sort = TRUE)
 
+count_loansWhenNeed <- count_loansWhenNeed %>%
+  pivot_wider(
+    names_from = Soum,
+    values_from = n,
+    values_fill = 0
+  ) %>%
+  rowwise() %>%
+  mutate(Total = sum(c_across(where(is.numeric)))) %>%
+  ungroup()
+
+print(count_loansWhenNeed, n = 60)
 
 
 
